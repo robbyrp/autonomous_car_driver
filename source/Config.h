e9cb41858634ac7732b394a1b2c8b82850b9ef64
Pana Robert-Ionut <114769621+robbyrp@users.noreply.github.com>
2025-08-18 15:40:54 +0300

    RACHETA SPATIALA

--- source/Config.h
+++ source/Config.h
@@ -6,14 +6,16 @@
 #define STEERING_P_LEFT       50U
 
 // Physical steering limits
-#define STEERING_LIMIT_RIGHT  30
-#define STEERING_LIMIT_LEFT  -30
+#define STEERING_LIMIT_RIGHT  55
+#define STEERING_LIMIT_LEFT  -55
 
 // Steering angle offset
-#define STEERING_OFFSET      -20
+#define STEERING_OFFSET      -24
 
 // Wheel speeds
-#define SPEED_RIGHT           40
-#define SPEED_LEFT            -40
+#define SPEED_TURN_LEFT           25
+#define SPEED_TURN_RIGHT         -30
+#define SPEED_LEFT           10
+#define SPEED_RIGHT         -10
 
 #endif
